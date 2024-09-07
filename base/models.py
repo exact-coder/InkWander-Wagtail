8c2aa08592c22be668c773ebb35b76e657f089af
exact-coder <hasanakash799@gmail.com>
2024-09-08 01:28:05 +0600

    feta: added contract page

--- base/models.py
+++ base/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from modelcluster.fields import ParentalKey
 from wagtail.admin.panels import (
     FieldPanel,
     MultiFieldPanel,
-
-    # import PublishingPanel:
+    FieldRowPanel,
+    InlinePanel,
     PublishingPanel,
 )
 # import RichTextField:
@@ -15,6 +16,9 @@ from wagtail.models import (
     RevisionMixin,
     TranslatableMixin,
 )
+from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField
+
+from wagtail.contrib.forms.panels import FormSubmissionsPanel
 # import register_snippet:
 from wagtail.snippets.models import register_snippet
 
@@ -73,3 +77,24 @@ class FooterText(
 
     class Meta(TranslatableMixin.Meta):
         verbose_name_plural = "Footer Text"
+
+class FormField(AbstractFormField):
+    page = ParentalKey('FormPage', on_delete=models.CASCADE, related_name='form_fields')
+
+class FormPage(AbstractEmailForm):
+    intro = RichTextField(blank=True)
+    thank_you_text = RichTextField(blank=True)
+
+    content_panels = AbstractEmailForm.content_panels + [
+        FormSubmissionsPanel(),
+        FieldPanel('intro'),
+        InlinePanel('form_fields', label="Form fields"),
+        FieldPanel('thank_you_text'),
+        MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel('from_address'),
+                FieldPanel('to_address'),
+            ]),
+            FieldPanel('subject'),
+        ],"Email"),
+    ]
