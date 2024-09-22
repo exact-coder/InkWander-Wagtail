0bc9e4bb7523fc4d53f189be0151354328990057
exact-coder <hasanakash799@gmail.com>
2024-09-23 01:15:01 +0600

    feta: added Carousel in the home page

--- home/models.py
+++ home/models.py
@@ -2,9 +2,29 @@ from django.db import models
 
 from wagtail.models import Page
 from wagtail.fields import RichTextField
+from modelcluster.fields import ParentalKey
+from wagtail.models import Orderable
 
 # import MultiFieldPanel:
-from wagtail.admin.panels import FieldPanel, MultiFieldPanel
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel,InlinePanel
+
+class HomePageCarousels(Orderable):
+    page = ParentalKey("home.HomePage", related_name="home_page_carousels")
+    carousel_image = models.ForeignKey("wagtailimages.Image",null=True,blank=False,on_delete=models.SET_NULL, related_name="+")
+    carousel_title = models.CharField(null=True,blank=False,max_length=200,help_text="Carousel Title *")
+    carousel_subtitle = models.CharField(null=True,blank=False,max_length=200,help_text="Carousel Subtitle")
+    carousel_cta = models.CharField(blank=True,null=True,verbose_name="Carousel CTA",max_length=255,help_text="Text to display on Call to Action",)
+    carousel_cta_link = models.ForeignKey("wagtailcore.Page",null=True,blank=True,on_delete=models.SET_NULL,help_text="Choose a page to link to for the Call to Action")
+    carousel_cta_url = models.URLField(verbose_name="Carousel URL", blank=True,help_text="If carousel_cta_link not found. Then this url work")
+
+    panels = [
+        FieldPanel("carousel_image"),
+        FieldPanel("carousel_title"),
+        FieldPanel("carousel_subtitle"),
+        FieldPanel("carousel_cta"),
+        FieldPanel("carousel_cta_link"),
+        FieldPanel("carousel_cta_url"),
+    ]
 
 
 class HomePage(Page):
@@ -41,6 +61,7 @@ class HomePage(Page):
 
     # modify your content_panels:
     content_panels = Page.content_panels + [
+        
         MultiFieldPanel(
             [
                 FieldPanel("image"),
@@ -50,5 +71,17 @@ class HomePage(Page):
             ],
             heading="Hero section",
         ),
-        FieldPanel('body'),
+        MultiFieldPanel(
+            [
+                InlinePanel("home_page_carousels",max_num=3,min_num=1,label="Carousel"),
+            ],
+            heading="Home Page Carousel section",
+        ),
+        MultiFieldPanel(
+            [
+                FieldPanel('body'),
+            ],
+            heading="Body section"
+        ),
+        
     ]
